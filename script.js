3488c79cec484ae80d8c15f58e4c094629c42f85
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2024-07-21 16:22:31 -0300

    Se hicieron modificaciones en el diseño y se añadio la posibilidad de elegir entre dos modos para cambiar los colores

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Funcion para crear un hexadecimal random
 let randomHex = () => {
     let random = '#';
     for (let i = 0; i < 6; i++) {
@@ -7,12 +8,50 @@ let randomHex = () => {
     return random;
 }
 
+//arreglo de colores para el modo de seleccion simple
+const colors = ["green", "red", "yellow", "rgba(133, 122, 200)", "lightgreen", "#f15025"];
+
+//Funcion para elegir un color aleatoriamente de la lista de colores
+let simpleRandomColor = () => {
+    let randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+//obteniedo el elemento div donde se establece el color
 const background = document.getElementById('background');
+
+//obteniedo el elemento span donde se establece el nombre del color
 const colorName = document.getElementById('colorName');
 
+//obteniendo los elementos p del nav para verificar el modo seleccionado
+const simpleMode = document.getElementById('simple-mode');
+const hexMode = document.getElementById('hex-mode');
+
+//Funcion para cambiar el modo activo de los modos
+const setActive = (mode) => {
+    if (mode === 'simple') {
+        simpleMode.classList.add('active');
+        hexMode.classList.remove('active');
+    } else {
+        simpleMode.classList.remove('active');
+        hexMode.classList.add('active');
+    }
+}
+
+//añadir un escuchador de eventos que escucha cuando se hace click y añade modo activo a ese elemento clickeado
+simpleMode.addEventListener('click', () => setActive('simple'));
+hexMode.addEventListener('click', () => setActive('hex'));
+
+//funcion para cambiar el color al hacer click en el botón dependiendo del modo que este seleccionado
 let flipColor = () => {
-    //obtener el color random
-    const color = randomHex();
+    let color = '';
+    //verificar que modo esta seleccionado
+    //si el modo es simple usar simpleRandomColor sino usar randonHex
+    if (simpleMode.classList.contains('active')) {
+        color = simpleRandomColor();
+    } else {
+        color = randomHex();
+    }
 
     //agregar el color al fondo 
     background.style.backgroundColor = color;
@@ -21,5 +60,5 @@ let flipColor = () => {
     colorName.textContent = color;
 }
 
-//llamamos la función al cargar la página
+//llamamos la función de elegir color al cargar la página
 flipColor();
